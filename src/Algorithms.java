9dca618e8900365b1b3839d5c08776dfb8e6911a
ktitova <ksunchik123@yandex.ru>
2022-05-04 14:04:09 +0300

    first commit

---
+++ src/Algorithms.java
@@ -0,0 +1,59 @@
+public class Algorithms {
+    /**
+     * Написать метод, который принимает на вход строку.
+     * Если строка не пустая (проверить методом из видео), то примените метод по удалению пробелов в начале строки и в конце строки.
+     * Догадаться, каким методом из видео можно проверить, были ли пробелы.
+     * Если пробелы были, то метод должен вернуть сообщение: “Лишние пробелы удалены”.
+     * Если пробелов не было, вернуть сообщение “Пробелов не было”.
+     * Если строка пустая, вернуть сообщение “Строка пустая”.
+     *
+     * Test Data:
+     * “    QA4Everyone   “ → “Лишние пробелы удалены”
+     * “QA4Everyone“ → “Пробелов не было”
+     * “” → “Строка пустая”
+     */
+
+    public static String deleteSpaces(String str) {
+        if (str.isEmpty() == false) {
+            String str1 = str.trim();
+            if (str.length() == str1.length()) {
+                return "Пробелов не было";
+            } else {
+                return "Лишние пробелы удалены";
+            }
+        }
+        return "Строка пустая";
+    }
+
+    /**Написать алгоритм RemoveAlla.
+      написать алгоритм, который принимает на вход строку.
+     Если строка валидная, то метод удаляет все буквы a из строки, если таковые имеются.
+     Метод возвращает обработанную строку.
+     Test Data:
+     “    QA4Everyone   “ →  “QA4Everyone“
+     “panda   “ → “pnd”
+     */
+
+    public static String removeAlla( String str){
+        str = str.trim();
+        if (str.isEmpty() == false) {
+            str = str.replace("a","");
+        }
+        return str;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(deleteSpaces("    QA4Everyone   "));
+        System.out.println(deleteSpaces("QA4Everyone"));
+        System.out.println(deleteSpaces(""));
+
+        System.out.println(removeAlla("    QA4Everyone   "));
+        System.out.println(removeAlla("panda   "));
+    }
+
+}
+
+/**
+ */
+
