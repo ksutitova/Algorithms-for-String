2a0c3aa7f8aa5142faf326243ead4914c2fc21bd
ktitova <ksunchik123@yandex.ru>
2022-05-04 14:15:55 +0300

    add algorithm

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -6,7 +6,7 @@ public class Algorithms {
      * Если пробелы были, то метод должен вернуть сообщение: “Лишние пробелы удалены”.
      * Если пробелов не было, вернуть сообщение “Пробелов не было”.
      * Если строка пустая, вернуть сообщение “Строка пустая”.
-     *
+     * <p>
      * Test Data:
      * “    QA4Everyone   “ → “Лишние пробелы удалены”
      * “QA4Everyone“ → “Пробелов не было”
@@ -25,19 +25,40 @@ public class Algorithms {
         return "Строка пустая";
     }
 
-    /**Написать алгоритм RemoveAlla.
-      написать алгоритм, который принимает на вход строку.
-     Если строка валидная, то метод удаляет все буквы a из строки, если таковые имеются.
-     Метод возвращает обработанную строку.
-     Test Data:
-     “    QA4Everyone   “ →  “QA4Everyone“
-     “panda   “ → “pnd”
+    /**
+     * Написать алгоритм RemoveAlla.
+     * написать алгоритм, который принимает на вход строку.
+     * Если строка валидная, то метод удаляет все буквы a из строки, если таковые имеются.
+     * Метод возвращает обработанную строку.
+     * Test Data:
+     * “    QA4Everyone   “ →  “QA4Everyone“
+     * “panda   “ → “pnd”
      */
 
-    public static String removeAlla( String str){
+    public static String removeAlla(String str) {
         str = str.trim();
         if (str.isEmpty() == false) {
-            str = str.replace("a","");
+            str = str.replace("a", "");
+        }
+        return str;
+    }
+
+
+    /**
+     * Написать алгоритм RemoveAllZeros.
+     * алгоритм, который принимает на вход строку,
+     * состоящую из цифр. Если строка валидная, то метод удаляет все пробелы из строки,
+     * если таковые имеются. Метод возвращает обработанную строку, в которой нет нулей.
+     * Test Data:
+     * “   3 5 0 4 2 0 9 7 0 6 0 4 0       0 0 0 “ →  “35429764“
+     * “ 0000000111“ → “111”
+     */
+
+    public static String removeAllZeros(String str) {
+        str = str.trim();
+        if (str.isEmpty() == false) {
+            str = str.replace(" ", "");
+            str = str.replace("0", "");
         }
         return str;
     }
@@ -50,10 +71,14 @@ public class Algorithms {
 
         System.out.println(removeAlla("    QA4Everyone   "));
         System.out.println(removeAlla("panda   "));
+
+        System.out.println(removeAllZeros("   3 5 0 4 2 0 9 7 0 6 0 4 0       0 0 0 "));
+        System.out.println(removeAllZeros(" 0000000111"));
     }
 
 }
 
 /**
+ *
  */
 
